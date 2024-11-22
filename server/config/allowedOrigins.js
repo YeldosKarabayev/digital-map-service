bbeaf3d16efac6c86a570bf9ab34edf90f86c299
Елдос Қарабаев <yeldos.k@evrika.com>
2024-11-22 22:58:41 +0500

    Update allowed

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigns = [
     'http://localhost:3000',
     'https://www.myspaceshop.com',
-    'https://digitalmaps.kz'
+    'https://digitalmaps.kz',
+    'http://91.147.92.212:3000'
 ]
 
 module.exports = allowedOrigns

843568458c3f2b156b495b7af588d0ec5ee37793
Елдос Қарабаев <yeldos.k@evrika.com>
2024-11-25 09:24:51 +0500

    Update build 25.11.2024 9:24

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -7,20 +7,3 @@ const allowedOrigns = [
 ]
 
 module.exports = allowedOrigns
-
-const corsOptions = {
-    origin: (origin, callback) => {
-        // Разрешаем запросы с указанных доменов или с отсутствующим origin (например, с сервера)
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);  // Разрешаем запросы с указанных доменов
-        } else {
-            callback(new Error('Not allowed by CORS'));  // Отклоняем запросы с других доменов
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Добавил OPTIONS для обработки предзапросов
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Разрешаем только указанные заголовки
-    credentials: true,  // Разрешение на передачу cookies и авторизации
-    optionsSuccessStatus: 200,  // Для обработки успешных preflight запросов
-};
-
-module.exports = corsOptions;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 
 require("dotenv").config()
+const fs = require('fs');
+const https = require('https');
 
 const express = require('express')
 const app = express()
